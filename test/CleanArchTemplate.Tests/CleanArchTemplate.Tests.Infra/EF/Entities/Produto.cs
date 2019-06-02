ef82e5017572282372cb623ef5dea6ea8371db37
Douglas Fernandes <douglasfpdasilvajunior@gmail.com>
2019-06-01 23:49:34 -0300

    Criar projeto de testes unitarios

---
+++ test/CleanArchTemplate.Tests/CleanArchTemplate.Tests.Infra/EF/Entities/Produto.cs
@@ -0,0 +1,13 @@
+﻿using CleanArchTemplate.Core.Entities.Base;
+
+namespace CleanArchTemplate.Tests.Infra.EF.Entities
+{
+	public class Produto : EntityBase
+	{
+		public int Id { get; set; }
+
+		public string Nome { get; set; }
+
+		public double Valor { get; set; }
+	}
+}
