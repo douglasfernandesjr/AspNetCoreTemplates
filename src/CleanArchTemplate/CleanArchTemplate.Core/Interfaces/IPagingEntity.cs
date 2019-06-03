46d2a3d37d8edfffb6ae9b0333ad13747969bb87
Douglas Fernandes <douglasfpdasilvajunior@gmail.com>
2019-06-02 23:04:46 -0300

    Criação de entidade padroes para paginação.

---
+++ src/CleanArchTemplate/CleanArchTemplate.Core/Interfaces/IPagingEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchTemplate.Core.Interfaces
+{
+	public interface IPagingEntity<T>
+		where T : IEntity
+	{
+	}
+}
---
+++ src/CleanArchTemplate/CleanArchTemplate.Core/Interfaces/IRepositoryPaging.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace CleanArchTemplate.Core.Interfaces
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public interface IRepositoryPaging<T> : IRepository<T>
+		where T : IEntity
+	{
+		IPagingEntity<T> Paging<TProp>();
+	}
+}
